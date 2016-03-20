8bc3ff0bba2678c2dbda8da87b76c1f69e766761
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-03-20 14:49:28 +0100

    :bug: Fix namespace in basic web app template

--- src/webbasic/Controllers/HomeController.cs
+++ src/webbasic/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
-namespace webbasic.Controllers
+namespace WebApplicationBasic.Controllers
 {
     public class HomeController : Controller
     {
